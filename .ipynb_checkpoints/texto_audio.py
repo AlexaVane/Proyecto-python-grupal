795a30c592d40239a6c8c65398c6b04e75458dec
AlexaVane <alexavanemacas321@gmail.com>
2020-02-04 12:59:46 -0500

    a

--- .ipynb_checkpoints/texto_audio.py
+++ .ipynb_checkpoints/texto_audio.py
@@ -10,5 +10,4 @@ from PyQt5 import QtCore, QtGui
 #tts = gTTS('Hola mundo.', lang='es-us')
 
 #with open("hola_mundo.wav", "wb") as archivo:
- #   tts.write_to_fp(archivo)
-
+ #   tts.write_to_fp(archivo)s
